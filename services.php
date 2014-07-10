ffef4860663150abec5db922782854b700f519ef
Brady Catherman <brady@orchestrate.io>
2014-07-10 12:38:53 -0600

    Bump the wordpress version and fix whitespace.

--- services.php
+++ services.php
@@ -124,9 +124,8 @@ function hookpress_ajax_get_hooks() {
 	if ($_POST['type'] == 'filter')
 		$hooks = array_keys($hookpress_filters);
 
-  header("Content-Type: text/html; charset=UTF-8");
+	header("Content-Type: text/html; charset=UTF-8");
 
-<<<<<<< HEAD
 	if (is_array($hooks)) {
 		sort($hooks);
 		foreach ($hooks as $hook) {
@@ -135,14 +134,4 @@ function hookpress_ajax_get_hooks() {
 		}
 	}
 	exit;
-=======
-  if (is_array($hooks)) {
-    sort($hooks);
-    foreach ($hooks as $hook) {
-      $hook = esc_html( $hook );
-      echo "<option value='$hook'>$hook</option>";
-    }
-  }
-  exit;
->>>>>>> Clean up some error logs
 }
